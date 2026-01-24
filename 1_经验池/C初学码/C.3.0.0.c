dc62ae4e1a2258d1c68c9cdf43cba1226477dd79
Chayer2333 <3936590541@qq.com>
2026-01-24 12:02:42 +0800

    提示编译器差异造成的不同

--- 1_经验池/C初学码/C.3.0.0.c
+++ 1_经验池/C初学码/C.3.0.0.c
@@ -2,6 +2,7 @@
 int main()
 {
 	int a,b,c;
+	printf("请输入俩个待交换的整数：\n");
 	scanf("%d%d",&a,&b);
 	c=a;
 	a=b;
@@ -10,7 +11,7 @@ int main()
 	a=a+b;
 	b=a-b;
 	a=a-b;
-	printf("%d,%d\n",a,b);
+	printf("%d,%d\n",a,b);	// 该方式有可能导致算数溢出或精度丢失
 	return 0;
 }
 
