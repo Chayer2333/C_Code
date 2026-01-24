b8832c51ec91c45d4a989b2a04c04e20fb1d886f
Chayer2333 <3936590541@qq.com>
2026-01-24 12:04:46 +0800

    改进注释

--- 1_经验池/C初学码/C.2.5.0.c
+++ 1_经验池/C初学码/C.2.5.0.c
@@ -9,7 +9,7 @@ int main()
 	fflush(stdin);//清屏
 	printf("请按格式输入字符ch1,ch2\n");
 	ch1=getchar(); 
-	ch3=getchar();//字符不是字符串
+	ch3=getchar();//字符不是字符串		// 注意！该代码在C89/C90标准与MSVC编译的程序执行方式不同，后者不适用提示的输入规则！
     ch2=getchar();
 	printf("a=%.2f,b=%.3f\n",a,b);
 	printf("i=%d,j=%d\n",i,j);
